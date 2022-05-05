e9efb2f6af2643a7aa02bb91f39580d0b8d05048
gw98-github <grzegorz.wazny.pv@gmail.com>
2022-05-05 11:52:22 +0200

    env apiUrl change

--- code/src/environments/environment.prod.ts
+++ code/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://156.17.135.29:8888/wordnetloom-server/resources/'
+  apiUrl: 'https://polonjid-api.clarin-pl.eu/wordnetloom-server/resources/'
 };
